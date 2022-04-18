0c7e68c2b61e05ed106772b853ac5161d30baf68
sabbir-mahmud <sabbir.mahmud.zim@gmail.com>
2022-04-18 08:48:06 +0600

    user profile components created

--- src/Components/Shared/Navbar/Navbar.js
+++ src/Components/Shared/Navbar/Navbar.js
@@ -32,12 +32,6 @@ const Navbar = () => {
                                 <CustomLink to="/">Home</CustomLink>
                             </li>
                             <li>
-                                <CustomLink to="/#service-container">Services</CustomLink>
-                            </li>
-                            <li>
-                                <CustomLink to="/#price-list">Prices</CustomLink>
-                            </li>
-                            <li>
                                 <CustomLink to="/checkout">Checkout</CustomLink>
                             </li>
                             <li>
@@ -45,7 +39,7 @@ const Navbar = () => {
                             </li>
 
                             {
-                                user ? <button onClick={handleLogout} className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sign out</button> : <CustomLink to="/login">Sign In</CustomLink>
+                                user.uid ? <button onClick={handleLogout} className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Sign out</button> : <CustomLink to="/login">Sign In</CustomLink>
                             }
                         </ul>
                     </div>
